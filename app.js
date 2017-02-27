ebe4d99a5ef18fecdba4cc65bacf2ac0ad4d3b26
Tim Nguyen <tungnh91@gmail.com>
2017-02-26 18:34:09 -0800

    Complete

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hovering: false
+    }
+  }
+
+  onListItemMouseEnter() {
+    this.setState({
+      hovering: true
+    });
+  }
+
+  onListItemMouseLeave() {
+    this.setState({
+      hovering: false
+    })
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.hovering ? 'bold' : 'normal'
+    };
+
+    return (
+      <li
+        style={style}
+        onMouseLeave={this.onListItemMouseLeave.bind(this)}
+        onMouseEnter={this.onListItemMouseEnter.bind(this)}
+      >
+        {this.props.item}
+      </li>
+    )
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+ReactDOM.render(<GroceryList items={['kale', 'cucumbers']} />, document.getElementById('app'));
